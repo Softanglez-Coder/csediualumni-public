1f9915eda75efbdb83261d869c76a22d39126bd5
Mehedi Hasan <mehedi.hasan23@brainstation-23.com>
2025-12-01 15:37:29 +0600

    refactor: remove authentication components and styles
    - Deleted AuthLayoutComponent and its associated styles.
    - Removed LoginComponent, its template, and styles.
    - Removed RegisterComponent, its template, and styles.
    - Updated index.html to include Font Awesome for icons.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,3 @@
 import { Routes } from '@angular/router';
-import { AuthLayoutComponent } from './layout/auth-layout/auth-layout.component';
-import { LoginComponent } from './modules/auth/login/login.component';
 
-export const routes: Routes = [
-  {
-    path: 'auth',
-    component: AuthLayoutComponent,
-    children: [
-      { path: 'login', component: LoginComponent },
-      { path: '', redirectTo: 'login', pathMatch: 'full' },
-    ],
-  },
-  {
-    path: '',
-    redirectTo: 'auth/login',
-    pathMatch: 'full',
-  },
-];
+export const routes: Routes = [];
